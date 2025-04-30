9fe714dbef5d822f9d6b7407cb87cbb3dc5a719c
jerome <2346784@cegeplimoilou.ca>
2025-04-29 22:30:49 -0400

    classe Client

--- TP3RJ/src/main/java/org/example/Client.java
+++ TP3RJ/src/main/java/org/example/Client.java
@@ -1,4 +1,22 @@
 package org.example;
+import javax.persistence.*;
 
+
+@Entity
+@Table(name = "Client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_client")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String nom;
+
+    @Column(nullable = false, length = 25)
+    private String courriel;
+
+    @Column(name = "mot_de_passe", nullable = false, length = 25)
+    private String motDePasse;
 }
